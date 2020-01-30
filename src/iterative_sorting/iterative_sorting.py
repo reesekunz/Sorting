b83ee851137ff94d91e1f9e1e2b883b266fce233
Reese Kunz <reesekunz21@gmail.com>
2020-01-29 19:46:03 -0700

    selection sort test passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,45 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+# Selction sort - first select min value in array and move it to far left,
+# then select 2nd smallest value in array and move it to index [1]
+# then select 3rd smallest value in array and move it to index [2]
+# etc..
+
+# Example: [3, 2, 4, 1]
+# 1. [1, 2, 4, 3] => 1 and 3 are swapped because 1 is min
+# 2. [1, 2, 4, 3] => 2 is in right place
+# 3. [1, 2, 3, 4] => 3 and 4 swap because 3 is 3rd smallest value
+
+# PLANNING:
+# Need an inner loop
+# Find the smallest element in the entire loop and place it at index[0]. Swap values.
+# Find the next smallest element and place it at index[1]. Swap values.
+# etc...
 
 
-
-        # TO-DO: swap
-
-
-
+def selection_sort(arr):
+    for x in range(0, len(arr) - 1):  # -1 since we start at index 0
+        print("arr", arr)
+        # print("x", x)
+        # cur_index = x (?)
+        # smallest_index = cur_index (?)
+        # Inner loop is the "function" we are having the loop do to compare values when the array is being looped through
+        # x+1 to check the index value to the right of x
+        for next_value in range(x+1, len(arr)):
+            # TO-DO: find next smallest element
+            # (hint, can do in 3 loc)
+            # Compare values - if next_value to the rightis smaller, then swap x and next_value indexes
+            if arr[next_value] < arr[x]:
+                arr[x], arr[next_value] = arr[next_value], arr[x]  # swap
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
