270725d8d26c914a326569c966de02a5f107c2fc
Reese Kunz <reesekunz21@gmail.com>
2020-01-29 20:46:23 -0700

    finished mvp for iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,24 +17,34 @@
 
 def selection_sort(arr):
     for x in range(0, len(arr) - 1):  # -1 since we start at index 0
-        print("arr", arr)
-        # print("x", x)
+        print("selection sort arr", arr)
+        print("selection sort outer loops", x)
         # cur_index = x (?)
         # smallest_index = cur_index (?)
         # Inner loop is the "function" we are having the loop do to compare values when the array is being looped through
         # x+1 to check the index value to the right of x
-        for next_value in range(x+1, len(arr)):
-            # TO-DO: find next smallest element
-            # (hint, can do in 3 loc)
-            # Compare values - if next_value to the rightis smaller, then swap x and next_value indexes
-            if arr[next_value] < arr[x]:
-                arr[x], arr[next_value] = arr[next_value], arr[x]  # swap
+
+        # TO-DO: find next smallest element (hint, can do in 3 loc)
+        for y in range(x+1, len(arr)):
+            print("selection sort inner loop", y)
+            # Compare values - if next_value to the right is smaller, then swap x and next_value indexes
+            if arr[y] < arr[x]:
+                arr[x], arr[y] = arr[y], arr[x]  # swap
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    for x in range(0, len(arr)-1):  # -1 since we start at index 0 - outer loop
+        # print("bubble sort arr", arr)
+        # print("bubble sort outer loops", x)
+        # inner loop. - x because we know the max value is already sorted at the end because they get 'bubbled'
+        for y in range(0, len(arr)-1 - x):
+            # print("bubble sort inner loop", y)
+            # compare pair of elements. if current index value > next index value, swap.
+            if arr[y] > arr[y+1]:
+                arr[y], arr[y+1] = arr[y+1], arr[y]
 
     return arr
 
